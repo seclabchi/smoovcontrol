42e675cecbc5a90ad044e46d6d574cca0b1c2af7
Paul Zaremba <zaremba@tonekids.com>
2022-09-17 12:11:52 -0500

    Initial Commit

---
+++ smoovcontrol/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  smoovcontrol
+//
+//  Created by Paul Zaremba on 9/17/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
